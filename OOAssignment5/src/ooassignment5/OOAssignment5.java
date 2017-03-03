f4e130434c1357f8e08ca9064e2b63eac6fdac95
walker-ger <pascal.engelbarts@gmail.com>
2017-03-03 12:41:49 +0100

    first

---
+++ OOAssignment5/src/ooassignment5/OOAssignment5.java
@@ -0,0 +1,17 @@
+
+package ooassignment5;
+
+/**
+ *
+ * @author pascal
+ */
+public class OOAssignment5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
